41876b074a4e531a6764fe6982739168c8fd3508
Rehmat Ali <itsrehmet@gmail.com>
2025-06-27 02:50:22 +0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base: '/koad/', // required for GitHub Pages
-  plugins: [    tailwindcss(),
-    react()],
+  base: '/koad/',
+  plugins: [tailwindcss(), react()],
 })
